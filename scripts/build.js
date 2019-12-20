419f3edce505fccbe0fa2cb51dcd0b9af9552b38
kinghoddy git config --global user.email odunmilade@gmail.com <you@example.com>
2019-12-20 18:14:32 +0100

    ddd

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,7 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
 // Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
+// ignoring them. In the future, promise rejections that are not handled willgi
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
   throw err;
